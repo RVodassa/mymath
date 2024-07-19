bae3a03d86624764f7e073f644e3e320898ea66d
assadov.spb <assadov.spb@bk.ru>
2024-07-19 17:01:13 +0300

    go

--- mymath.go
+++ mymath.go
@@ -1,1 +1,9 @@
+// Пример кода для иллюстрации задачи
 package mymath
+
+import "math"
+
+// Sqrt - функция для вычисления квадратного корня из числа
+func Sqrt(x float64) int {
+	return int(math.Sqrt(x))
+}
